11d4234fc879eb5722497aa5edbd64332634a633
Suryansh <74529332+suryanshtv@users.noreply.github.com>
2023-07-26 03:49:16 +0530

    do not include oplusextras

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_oplus
